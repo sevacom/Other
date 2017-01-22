3326f63faca13188fcc3dcf2aecc3838c292e416
sevacom <sevacom@hotmail.com>
2017-01-22 20:05:57 +0700

    change connection string

--- src/Samples.Dapper/Properties/Settings.Designer.cs
+++ src/Samples.Dapper/Properties/Settings.Designer.cs
@@ -22,16 +22,5 @@ namespace Samples.Dapper.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=SimpleDataBase.mdf;Integrated" +
-            " Security=True")]
-        public string SimpleDataBaseConnectionString {
-            get {
-                return ((string)(this["SimpleDataBaseConnectionString"]));
-            }
-        }
     }
 }
