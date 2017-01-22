b088de769fceda1a6922d6864e15a2877d950cac
sevacom <sevacom@hotmail.com>
2017-01-22 17:35:44 +0700

    Simple Asp.Net, Web Api

---
+++ src/Samples.AspNet.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Samples.AspNet.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src/Samples.AspNet.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Samples.AspNet.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
