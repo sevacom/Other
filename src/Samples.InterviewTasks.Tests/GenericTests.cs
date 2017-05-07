87b60ace7e61ef54d75a87353ceb5e73e5e2034c
sevacom <sevacom@hotmail.com>
2017-05-08 00:47:07 +0700

    Interning, Generic

---
+++ src/Samples.InterviewTasks.Tests/GenericTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Samples.InterviewTasks.Tests
+{
+    [TestFixture]
+    public class GenericTests
+    {
+        [Test]
+        public void ShouldOneStaticFieldForGenericWithSpecificType()
+        {
+            GenericClass<Model1>.StaticValue = 42;
+            GenericClass<Model2>.StaticValue.Should().NotBe(GenericClass<Model1>.StaticValue);
+
+            GenericClass<int>.StaticValue = 56;
+            GenericClass<uint>.StaticValue.Should().NotBe(GenericClass<int>.StaticValue);
+        }
+
+        private class GenericClass<T>
+        {
+            public static int StaticValue = 2;
+        }
+
+
+        private class Model1 { }
+            
+        private class Model2 { }
+    }
+}
---
+++ src/Samples.InterviewTasks.Tests/StringInterningTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Samples.InterviewTasks.Tests
+{
+    // https://blogs.msdn.microsoft.com/ericlippert/2009/09/28/string-interning-and-string-empty/
+    //If you are trying to reduce the total amount of memory your application allocates, keep in mind that interning a string has two unwanted side effects.
+    //First, the memory allocated for interned String objects is not likely be released until the common language runtime (CLR) terminates.
+    //The reason is that the CLR's reference to the interned String object can persist after your application, or even your application domain, terminates. 
+    //Second, to intern a string, you must first create the string. 
+    //The memory used by the String object must still be allocated, even though the memory will eventually be garbage collected.
+    [TestFixture]
+    public class StringInterningTests
+    {
+
+        [Test]
+        public void ShouldIntern()
+        {
+            object obj = "Int32";
+            string str1 = "Int32";
+            string str2 = typeof (int).Name;
+
+            (obj == str1).Should().BeTrue();
+            (str1 == str2).Should().BeTrue();
+
+            (obj == str2).Should().BeFalse();
+        }
+
+        [Test]
+        public void ShouldInternWhenEmpty()
+        {
+            object obj = "";
+            string str1 = "";
+            string str2 = string.Empty;
+
+            (obj == str1).Should().BeTrue();
+            (str1 == str2).Should().BeTrue();
+
+            // True - In the .NET Framework 1.0, .NET Framework 1.1, and .NET Framework 3.5 SP1 и выше
+            // False - В .NET Framework 2.0 с пакетом обновления 1 (SP1) и .NET Framework 3.0
+            (obj == str2).Should().BeTrue();
+        }
+    }
+}
